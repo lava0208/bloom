6ab22402beab78e9e72d9ee68076e1d6f49e5388
lava_0208 <lava_0208@hotmail.com>
2023-02-03 13:02:32 -0500

    update on 0203

--- components/masterplan/ByPlant.js
+++ components/masterplan/ByPlant.js
@@ -1,48 +1,62 @@
-import React from "react";
-import moment from "moment";
+import React, { useState } from "react";
 
 import { byPlant } from "~lib/dummy";
+import { Modal, ModalBody } from "reactstrap";
 
+import 'bootstrap/dist/css/bootstrap.css';
 import styles from "~styles/components/masterplan/byplant.module.scss";
 
-const ByPlant = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.currentPlantContainer}>
-        <div className={styles.plantDetailsContainer}>
-          <div className={styles.plantImage}></div>
-          <div className={styles.plantInfoContainer}>
-            <h3>{byPlant.currentPlant.name}</h3>
-            <h4>{byPlant.currentPlant.variety}</h4>
-            <h5>{byPlant.currentPlant.description}</h5>
-          </div>
-        </div>
-        <div className={styles.plantOptionsContainer}>
-          {byPlant.plantOptions.map((option, i) => (
-            <div className={styles.plantOptionRow} key={i}>
-              <h3>{option.name}</h3>
-              <h4>{moment(option.date).format("MMMM Do, YYYY")}</h4>
-              <h5>{option.status}</h5>
-            </div>
-          ))}
-          <button className={styles.editButton}>Edit</button>
-        </div>
-      </div>
+import ByPlantDetail from "./ByPlantDetail";
 
-      <div className={styles.plantsContainer}>
-        {byPlant.plants.map((plant, i) => (
-          <div className={styles.plantContainer} key={i}>
-            <div className={styles.plantImage}></div>
-            <div className={styles.plantInfoContainer}>
-              <h3>{plant.name}</h3>
-              <h4>{plant.variety}</h4>
-              <h5>{plant.description}</h5>
+const ByPlant = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const savePlant = () => {
+        setModalOpen(false);
+    }
+    const cancelPlant = () => {
+        setModalOpen(false);
+    }
+    const [activePlant, setActivePlant] = useState({});
+    return (
+        <div className={styles.container}>
+            <div className={styles.headerContainer}>
+                <h2>2023 Plan Plantings</h2>
+                <input className={styles.searchButton} placeholder={'Search'} />
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+            <div className={styles.plantsContainer}>
+                {byPlant.plants.map((plant, i) => (
+                    <div className={styles.plantContainer} key={i}>
+                        <div className={styles.plantImage}></div>
+                        <div className={styles.plantInfoContainer}>
+                            <div className={styles.plantInfoHeaderContainer}>
+                                <div className={styles.plantInfoHeader}>
+                                    <div>
+                                        <h3>{plant.name}</h3>
+                                        <h5>{plant.user}</h5>
+                                    </div>
+                                    <h4>{plant.unit}ct</h4>
+                                </div>
+                                <div className={styles.plantOptionsContainer}>
+                                    <h5>Start</h5>
+                                    <h5>Regular</h5>
+                                    <h5>Pinch</h5>
+                                    <h5>Pot On</h5>
+                                </div>
+                            </div>
+                            <div className={styles.plantInfoFooterContainer}>
+                                <button onClick={() => {setModalOpen(true); setActivePlant(plant)}}>View & Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen} centered modalClassName="modifyPlanModal">
+                <ModalBody>
+                    <ByPlantDetail activePlant={activePlant} savePlant={savePlant} cancelPlant={cancelPlant} />
+                </ModalBody>
+            </Modal>
+        </div>
+    );
 };
 
 export default ByPlant;
